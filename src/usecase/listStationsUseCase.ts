d543b7ab1277fcc5f929dbffc05d848096e9045b
Wesley-Arizio <wesleyarizio26@gmail.com>
2023-09-24 12:52:24 -0300

    implemented filtering for stations by planet

--- src/usecase/listStationsUseCase.ts
+++ src/usecase/listStationsUseCase.ts
@@ -1,12 +1,19 @@
 import { IUseCase } from ".";
 import { Station } from "../entities/station";
 import { IPagination, IRepository, RepositoryError } from "../repository";
+import { IStationRepository } from "../repository/stationRepository";
 
-export class ListStationsUseCase implements IUseCase<Station[], IPagination> {
-  constructor(private readonly stationRepository: IRepository<Station>) {}
-  async execute(args: IPagination): Promise<Station[]> {
+export interface IListStationsUseCaseArgs extends Partial<IPagination> {
+  planetId: string;
+}
+
+export class ListStationsUseCase
+  implements IUseCase<Station[], IListStationsUseCaseArgs>
+{
+  constructor(private readonly stationRepository: IStationRepository) {}
+  async execute(args: IListStationsUseCaseArgs): Promise<Station[]> {
     try {
-      return await this.stationRepository.getMany(args);
+      return await this.stationRepository.getStationsByPlanet(args);
     } catch (e) {
       if (e instanceof RepositoryError) {
         throw new Error("Internal Server Error");
